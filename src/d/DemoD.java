dbf42baf004eca870cd3675e62308dd153a60d95
Simaaaltmimi <155588293+Simaaaltmimi@users.noreply.github.com>
2024-01-03 21:54:57 -0500

    Update DemoD.java

--- src/d/DemoD.java
+++ src/d/DemoD.java
@@ -5,5 +5,14 @@ public class DemoD {
 		System.out.println("Ruby");
 		System.out.println("Rere");
 	}
+	//Sprint 2 automation
+	
+	public static void add(int a, int b) {
+		System.out.println(a+b);
+	}
+	//Sprint 2 divaid
+	
+	public static void divide(int a, int b) {
+		System.out.println(a/b)
 
 }
